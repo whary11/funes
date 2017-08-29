38f065c9245b6ea93106aa078026b1dcb7b2b505
David Raga Renteria <davidragarenteria@gmail.com>
2017-08-29 14:54:14 -0500

    nada

--- seis/modulos/presupuesto/index.php
+++ seis/modulos/presupuesto/index.php
@@ -116,8 +116,8 @@
       </div>
     </div>
 <body>
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/app/js/app.js"></script>
+<script src="js/jquery.min.js"></script>
+<script src="js/app.js"></script>
 <script src="js/dropzone.js"></script>
 <script src="js/accion_presupuesto.js"></script>
 
--- seis/modulos/presupuesto/listarproductos.php
+++ seis/modulos/presupuesto/listarproductos.php
@@ -1,13 +1,17 @@
 <?php 
 	 require_once("../../controladores/conexion/conn.php");
 	$db = new conexion();
-	
+	// $array = [];
 	$id = $_POST["id"];
-	$q = "SELECT precio FROM productos_funes WHERE (id='$id')";
+	$q = "SELECT precio,codigo FROM productos_funes WHERE (id='$id')";
 	$data = $db->leeTabla($q);
 
 		for ($i=0; $i <count($data); $i++) {
 			$precio = $data[$i]->precio;
+			$codigo = $data[$i]->codigo;
 		}
 		print($precio);
+		// $array = array(0=> $precio, 1 => $codigo );
+		// echo json_encode($array);
+		// var_dump($array);
  ?>
