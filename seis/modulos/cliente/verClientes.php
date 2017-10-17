b11bb61c66966d81207ff22547c874298048a5f2
whary11 <whary11@gmail.com>
2017-10-16 21:13:50 -0500

    .....

--- seis/modulos/cliente/verClientes.php
+++ seis/modulos/cliente/verClientes.php
@@ -56,7 +56,8 @@
         <input type="hidden" name="">
     </div>
 </div>
-
+<style type="text/css">
+</style>
 <!-- Modal -->
 <div class="modal fade" id="modalcliente" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
